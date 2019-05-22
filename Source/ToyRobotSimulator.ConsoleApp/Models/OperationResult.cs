2bde5ec5adc752aaf9d2d3497d586259357af11a
arpix8 <fede92@email.it>
2019-05-22 21:55:15 +0200

    add

---
+++ Source/ToyRobotSimulator.ConsoleApp/Models/OperationResult.cs
@@ -0,0 +1,13 @@
+﻿namespace ToyRobotSimulator
+{
+    public class OperationResult
+    {
+        public bool HasErrors { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class OperationResult<T> : OperationResult
+    {
+        public T Result { get; set; }
+    }
+}
---
+++ Source/ToyRobotSimulator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ToyRobotSimulator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ToyRobotSimulator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9df540f8-f114-4304-8c83-ea33866303ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
